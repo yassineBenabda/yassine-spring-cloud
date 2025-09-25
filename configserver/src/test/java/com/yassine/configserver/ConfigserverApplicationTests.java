b1a51ed592370df7e6ac3499c93e67706717624a
yassineBenabda <benabdayassine395@gmail.com>
2025-09-25 01:42:55 +0100

    configserver

---
+++ configserver/src/test/java/com/yassine/configserver/ConfigserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yassine.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigserverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
